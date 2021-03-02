4a34a9e71f4ca06cd8060165a6b9bcf95cb659c3
HerpDerp <herpitehderpo@gmail.com>
2021-03-02 13:52:34 -0500

    have all the basics of initializing the project ready

---
+++ roads/initgrid.py
@@ -0,0 +1,17 @@
+
+
+def init(gridsize=8):
+    '''Creates the grid and calls for road creation'''
+    
+
+def create_road(grid):
+    '''Uses the grid and creates a connection from one random point to another
+    '''
+    pass
+    return grid
+
+def tie_up_loose_ends(grid):
+    '''Takes the grid and connects roads that lead to nowhere to some random
+    place. "The roads will the swimming with the fishes soon"'''
+    pass
+    return grid
